e270cdb725b212330267f380b8293793c86e1174
Uros Ognjenovic <urosognjenovic17@gmail.com>
2025-04-07 00:38:07 +0200

    Format with black

--- src/agent.py
+++ src/agent.py
@@ -3,24 +3,27 @@ from forta_agent import transaction_event, Finding, FindingType, FindingSeverity
 from typing import List
 
 AAVE_V3_ADDRESS: str = "0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2".lower()
-FLASH_LOAN_TOPIC: str = "0xefefaba5e921573100900a3ad9cf29f222d995fb3b6045797eaea7521bd8d6f0".lower()
+FLASH_LOAN_TOPIC: str = (
+    "0xefefaba5e921573100900a3ad9cf29f222d995fb3b6045797eaea7521bd8d6f0".lower()
+)
 # USDT
-PROTOCOL: str = "0xdAC17F958D2ee523a2206206994597C13D831ec7".lower() 
+PROTOCOL: str = "0xdAC17F958D2ee523a2206206994597C13D831ec7".lower()
 TARGET_PROTOCOLS: List[str] = [PROTOCOL]
 
+
 def handle_transaction(
-        transaction_event: transaction_event.TransactionEvent
-    ) -> List[transaction_event.TransactionEvent]:
+    transaction_event: transaction_event.TransactionEvent,
+) -> List[transaction_event.TransactionEvent]:
     """
     Takes transaction event and returns a list of findings that have a flash loan in it.
-    """    
-    findings: List[transaction_event.TransactionEvent] = [] # type: ignore
+    """
+    findings: List[transaction_event.TransactionEvent] = []  # type: ignore
 
     addresses_lowered = [key.lower() for key in transaction_event.addresses.keys()]
 
     if AAVE_V3_ADDRESS not in addresses_lowered:
         return findings
-    
+
     flash_loan_events = []
 
     for log in transaction_event.logs:
@@ -30,7 +33,7 @@ def handle_transaction(
 
     if len(flash_loan_events) == 0:
         return findings
-    
+
     for address in TARGET_PROTOCOLS:
         if address in addresses_lowered:
             findings.append(
@@ -41,7 +44,7 @@ def handle_transaction(
                         "alert_id": "FORTA-5",
                         "protocol": "AAVE",
                         "type": FindingType.Suspicious,
-                        "severity": FindingSeverity.Low
+                        "severity": FindingSeverity.Low,
                     }
                 )
             )
--- src/agent_test.py
+++ src/agent_test.py
@@ -3,53 +3,33 @@ from forta_agent import TransactionEvent, create_transaction_event
 from agent import handle_transaction, AAVE_V3_ADDRESS, PROTOCOL, FLASH_LOAN_TOPIC
 from typing import List
 
-mock_tx_dictionary = {
-    "transaction": {
-        "hash": "0x1234"
-    }, 
-    "addresses": {
-        "0x5678": True
-    }
-}
+mock_tx_dictionary = {"transaction": {"hash": "0x1234"}, "addresses": {"0x5678": True}}
 
 mock_tx_dictionary_for_USDT = {
-    "transaction": {
-        "hash": "0x1234"
-    }, 
-    "addresses": {
-        AAVE_V3_ADDRESS: True,
-        PROTOCOL: True
-    },
-    "logs": [
-        {
-            "topics": [FLASH_LOAN_TOPIC],
-            "address": AAVE_V3_ADDRESS
-        }
-    ]
+    "transaction": {"hash": "0x1234"},
+    "addresses": {AAVE_V3_ADDRESS: True, PROTOCOL: True},
+    "logs": [{"topics": [FLASH_LOAN_TOPIC], "address": AAVE_V3_ADDRESS}],
 }
 
-mock_tx_event: TransactionEvent = create_transaction_event(
-    mock_tx_dictionary
-) 
+mock_tx_event: TransactionEvent = create_transaction_event(mock_tx_dictionary)
 mock_tx_event.filter_log = Mock()
 
 mock_tx_event_for_USDT: TransactionEvent = create_transaction_event(
     mock_tx_dictionary_for_USDT
-) 
+)
 mock_tx_event_for_USDT.filter_log = Mock()
+
 
 class TestFlashLoanDetector:
     def test_returns_empty_if_no_aave_contract(self):
         findings: List[TransactionEvent] = handle_transaction(mock_tx_event)
         assert len(findings) == 0
-    
+
     def test_returns_empty_if_no_flash_loan_events(self):
-        mock_tx_event.addresses = {
-            AAVE_V3_ADDRESS: True
-        }
+        mock_tx_event.addresses = {AAVE_V3_ADDRESS: True}
         findings: List[TransactionEvent] = handle_transaction(mock_tx_event)
         assert len(findings) == 0
-    
+
     def test_returns_finding_in_a_flash_loan(self):
         findings: List[TransactionEvent] = handle_transaction(mock_tx_event_for_USDT)
         assert len(findings) == 1
